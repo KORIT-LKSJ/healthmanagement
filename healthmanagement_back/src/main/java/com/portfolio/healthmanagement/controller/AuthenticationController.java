60092efd961b61146c853a31fc8a8b9dc2765605
heehee <heeheer_g@naver.com>
2023-05-08 15:04:03 +0900

    Feat/ 토큰 인증확인 구현완료

--- healthmanagement_back/src/main/java/com/portfolio/healthmanagement/controller/AuthenticationController.java
+++ healthmanagement_back/src/main/java/com/portfolio/healthmanagement/controller/AuthenticationController.java
@@ -4,14 +4,17 @@ import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.portfolio.healthmanagement.aop.annotation.ValidAspect;
 import com.portfolio.healthmanagement.dto.auth.LoginReqDto;
 import com.portfolio.healthmanagement.dto.auth.registerReqDto;
 import com.portfolio.healthmanagement.service.AuthenticationService;
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,8 +25,8 @@ public class AuthenticationController {
 	private final AuthenticationService authenticationService;
 	
 	@ValidAspect
-	@PostMapping("/auth/singup")
-	public ResponseEntity<?> singup(@Valid @RequestBody  registerReqDto registerReqDto, BindingResult bindingResult){
+	@PostMapping("/auth/signup")
+	public ResponseEntity<?> signup(@Valid @RequestBody  registerReqDto registerReqDto, BindingResult bindingResult){
 		
 		authenticationService.checkDuplicatedUsername(registerReqDto.getUsername());
 		authenticationService.register(registerReqDto);
@@ -36,6 +39,16 @@ public class AuthenticationController {
 	public ResponseEntity<?> login(@Valid @RequestBody LoginReqDto loginReqDto, BindingResult bindingResult){
 		System.out.println(loginReqDto);
 		return ResponseEntity.ok().body(authenticationService.login(loginReqDto));
+	}
+	@GetMapping("/auth/authenticated")
+	public ResponseEntity<?> authenticated(@RequestParam String accessToken){
+		return ResponseEntity.ok().body(authenticationService.authenticated(accessToken));
+		
+	}
+	
+	@GetMapping("/auth/principal")
+	public ResponseEntity<?>principal( @RequestParam String accessToken){
+		return ResponseEntity.ok().body(authenticationService.getPrincipal(accessToken));
 	}
 }
 
--- healthmanagement_back/src/main/java/com/portfolio/healthmanagement/service/AuthenticationService.java
+++ healthmanagement_back/src/main/java/com/portfolio/healthmanagement/service/AuthenticationService.java
@@ -1,15 +1,17 @@
 package com.portfolio.healthmanagement.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
 
 import com.portfolio.healthmanagement.dto.auth.LoginReqDto;
 import com.portfolio.healthmanagement.dto.auth.registerReqDto;
 import com.portfolio.healthmanagement.dto.response.JwtRespDto;
+import com.portfolio.healthmanagement.dto.response.PrincipalRespDto;
 
 
 public interface AuthenticationService {
 	
 	public void checkDuplicatedUsername(String username);
 	public void register(registerReqDto registerReqDto);
+	public boolean authenticated(String accessToken);
+	public PrincipalRespDto getPrincipal(String accessToken);
 	public JwtRespDto login(LoginReqDto loginReqDto);
 }
