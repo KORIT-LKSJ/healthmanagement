9d38b4fc26e0393779ac8c4d636cc5b27dcc36d1
DM2735 <quseis@naver.com>
2023-05-23 09:56:43 +0900

    Feat: LikeList페이지 디자인 수정

--- healthmanagement_front/src/pages/LikeList/LikeList.js
+++ healthmanagement_front/src/pages/LikeList/LikeList.js
@@ -1,95 +1,181 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import axios from "axios";
-import React from "react";
-import { QueryClient, useQuery } from "react-query";
-import { useNavigate, useParams } from "react-router-dom";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { QueryClient, useQuery } from 'react-query';
+import { useNavigate, useParams } from 'react-router-dom';
+import Header from "../../components/Main/Header/Header";
+import Footer from "../../components/Main/Footer/Footer";
 
 const container = css`
     display: flex;
     flex-direction: column;
     align-items: center;
-    font-size: 12px;
 `;
 
-const table = css`
+const mainContainer = css`
+     position: relative;
+    top: 5%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    border: 1px solid white;
-    background-color: white;
     width: 40%;
-`;
-const thAndTdTitle = css`
+    height: 90%;
+    background-color: white;
+    overflow: auto;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+`
+const cardContainer = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1%;
+    margin-bottom: 6%;
+    margin-top: 2%;
+    border: 1px solid #dbdbdb;
+    border-radius: 7px;
+    box-shadow: 0px 0px 5px #dbdbdb;
+    width: 75%;
+    height: 100%;
     cursor: pointer;
     &:hover {
-        text-shadow: 0px 5px 10px #dbdbdb;
-        color: #dbdbdb;
+        box-shadow: 0px 0px 10px #dbdbdb;
     }
     &:active {
         background-color: #fafafa;
     }
 `;
 
-const thAndTd = css`
+const gymListContainer = css`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding: 0 5%;
+    width: 100%;
+    height: 400px;
+`
+const header = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 4%;
+    width: 100%;
+    height: 5%;
+`;
+
+const titleText = css`
+    font-weight: 600;
+`;
+const main = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 4%;
+    width: 100%;
+    height: 55%;
+`;
+
+const imgBox = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 7px;
+    box-shadow: 0px 5px 5px #dbdbdb;
+    padding: 5px;
+    width: 90%;
+    height: 100%;
+    background-color: #fafafa;
+    overflow: hidden;
+`;
+const img = css`
+    width: 100%;
+    height: 100%;
+`;
+
+const footer = css`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    font-weight: 600;
+    font-size: 14px;
+    width: 90%;
+    height: 30%;
+    padding-bottom: 5%;
+`;
+
+const infoDetail = css`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`;
+
+const like = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     border: 1px solid #dbdbdb;
-    padding: 5px 10px;
-    text-align: center;
+    border-radius: 7px;
+    padding: 5%;
+    height: 30px;
+    background-color: white;
+    font-weight: 600;
+    box-shadow: 0px 5px 5px #dbdbdb;
 `;
 
 const LikeList = () => {
-    const { userId } = useParams();
-    console.log(userId);
+    const [refresh, setRefresh] = useState(false);
+    const {userId} = useParams();
+    console.log(userId)
     const navigate = useNavigate();
 
     const likeGyms = useQuery(["likeGyms"], async () => {
         const option = {
             headers: {
-                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-        };
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+            }
+        }
         return await axios.get(`http://localhost:8080/gym/${userId}/like/list`, option);
     });
 
     if (likeGyms.isLoading) {
         return <div>로딩중...</div>;
     }
-
-    const titleClickHandle = (e) => {};
-
+    
     return (
         <div css={container}>
-            <table css={table}>
-                <thead></thead>
-                <tbody>
-                    <tr>
-                        <th css={thAndTd}>헬스장 명</th>
-                        <th css={thAndTd}>위치</th>
-                        <th css={thAndTd}>전화번호</th>
-                        <th css={thAndTd}>가격</th>
-                    </tr>
-                    {likeGyms.data.data.map((likeGym) => {
-                        return (
-                            <tr key={likeGym.gymId}>
-                                <td css={thAndTd}>
-                                    <div
-                                        css={thAndTdTitle}
-                                        onClick={() => {
-                                            navigate("/gym/" + likeGym.gymId);
-                                        }}
-                                    >
-                                        {likeGym.gymName}
-                                    </div>
-                                </td>
-                                <td css={thAndTd}>{likeGym.gymAddress}</td>
-                                <td css={thAndTd}>{likeGym.gymTel}</td>
-                                <td css={thAndTd}>{likeGym.gymPrice}￦</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <Header search={false}></Header>
+            <div css={mainContainer}>
+                <div css={gymListContainer}>
+                    {likeGyms.data.data.map(likeGym => {
+                    return(
+                        <>
+                        <div css={cardContainer} onClick={()=>{navigate(("/gym/" + likeGym.gymId))}}>
+                            <header css={header}>
+                                <h1 css={titleText}>{likeGym.gymName}</h1>
+                            </header>
+                            <main css={main}>
+                                <div css={imgBox}>
+                                    <img css={img} src={likeGym.gymImgUrl} />
+                                </div>
+                            </main>
+                            <footer css={footer}>
+                                <h2 css={infoDetail}>위치: {likeGym.gymAddress} </h2>
+                                <h2 css={infoDetail}>가격: (월) {likeGym.gymPrice}&#8361;</h2>
+                                <h2 css={infoDetail}> ☎ {likeGym.gymTel}</h2>
+                            </footer>
+                        </div>
+                    </>
+                    )
+                })}
+            </div>
         </div>
+        <Footer/>
+    </div>
     );
 };
 
