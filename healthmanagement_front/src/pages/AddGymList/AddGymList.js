59debbc1464d3aa19283df4dd01e6be89076f865
heehee <heeheer_g@naver.com>
2023-05-25 14:11:44 +0900

    Fix/ 헬스장등록 리스트 구현중

--- healthmanagement_front/src/pages/AddGymList/AddGymList.js
+++ healthmanagement_front/src/pages/AddGymList/AddGymList.js
@@ -1,11 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import React from "react";
-import Header from "../../components/Main/Header/Header";
-import Footer from "../../components/Main/Footer/Footer";
-import { useNavigate } from "react-router-dom";
-import { useQuery } from "react-query";
 import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+import Footer from "../../components/Main/Footer/Footer";
+import Header from "../../components/Main/Header/Header";
 
 const container = css`
   display: flex;
@@ -30,50 +29,25 @@ const mainContainer = css`
 `;
 
 const addlistcontainer = css`
-  flex-direction: column;
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 1px solid white;
   background-color: white;
-  width: 100px;
+  width: 800px;
   height: 100px;
-`;
 
-const titlebox = css`
-  display: flex;
-  flex-direction: column;
-  border-style: double;
-  border: 1px solid black;
-`;
+  & th,
+  td {
+    width: 150px;
+    height: 30px;
+    text-align: center;
+    vertical-align: middle;
 
-const companyName = css`
-  display: flex;
-  width: 33.3%;
-  justify-content: center;
-  border: black;
-`;
-
-const thAndTdTitle = css`
-  cursor: pointer;
-  &:hover {
-    text-shadow: 0px 5px 10px #dbdbdb;
-    color: #dbdbdb;
+    border: 1px solid #dbdbdb;
   }
-  &:active {
-    background-color: #fafafa;
-  }
-`;
-
-const thAndTd = css`
-  border: 1px solid #dbdbdb;
-  padding: 5px 10px;
-  text-align: center;
 `;
 
 const AddGymList = () => {
-  const navigate = useNavigate();
-
   const principal = useQuery(["principal"], async () => {
     const option = {
       headers: {
@@ -116,36 +90,28 @@ const AddGymList = () => {
     <div css={container}>
       <Header search={false} />
       <main css={mainContainer}>
-        <tobody css={addlistcontainer}>
-          {addGyms.isLoading
-            ? ""
-            : addGyms.data.data.map((addGym) => {
-                return (
-                  <tr key={addGym.gymId}>
-                   
-                      <h2 css={companyName}> 이름 </h2>
-                      <h2> 주소</h2>
-                      <h2> 전화번호 </h2>
-                      <h2> 가격 </h2>
-                    
-                    <div css={thAndTd}>
-                      <div
-                        css={thAndTdTitle}
-                        onClick={() => {
-                          navigate("/gym/" + addGym.gymId);
-                        }}
-                      >
-                        {addGym.gymName}
-                      </div>
-                    </div>
-
-                    <h2 css={thAndTd}>{addGym.gymAddress}</h2>
-                    <h2 css={thAndTd}>{addGym.gymTel}</h2>
-                    <h2 css={thAndTd}>{addGym.gymPrice}￦</h2>
-                  </tr>
-                );
-              })}
-        </tobody>
+        <table css={addlistcontainer}>
+          <tbody>
+            <tr>
+              <th> 이름 </th>
+              <th> 주소</th>
+              <th> 전화번호 </th>
+              <th> 가격 </th>
+            </tr>
+            {addGyms.isLoading
+              ? ""
+              : addGyms.data.data.map((addGym) => {
+                  return (
+                    <tr>
+                      <td>{addGym.gymName}</td>
+                      <td>{addGym.gymAddress}</td>
+                      <td>{addGym.gymTel}</td>
+                      <td>{addGym.gymPrice}</td>
+                    </tr>
+                  );
+                })}
+          </tbody>
+        </table>
       </main>
       <Footer />
     </div>
