4492733146130fe0fcd7c47f0b359218f65a0728
DM2735 <quseis@naver.com>
2023-05-31 15:17:29 +0900

    Revert "Revert "Feat : GymDetail 페이지에 사진보이게 구현, 사진 클릭 시 모달창 띄워서 사진 목록 보이기, 메인 화면 헬스장 리스트에서 헬스장 사진들 보이게 구현.""
    This reverts commit 61b2d9a443e975c34ff853d973d3081632757ec3.

--- healthmanagement_front/src/pages/GymDetail/GymDetail.js
+++ healthmanagement_front/src/pages/GymDetail/GymDetail.js
@@ -1,19 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
 import Header from "../../components/Main/Header/Header";
 import Footer from "../../components/Main/Footer/Footer";
-
+import GymImgs from "../../components/GymImgs/GymImgs";
 const container = css`
     display: flex;
     flex-direction: column;
     align-items: center;
     height: 100%;
 `;
-
+const gymImg = css`
+    width: 750px;
+`;
 const header = css`
     display: flex;
     justify-content: space-between;
@@ -39,9 +41,6 @@ const main = css`
     }
 `;
 
-const gymImg = css`
-    width: 750px;
-`;
 const detailName = css`
     display: flex;
     flex-direction: column;
@@ -71,6 +70,16 @@ const likeIcon = css`
 const GymDetail = () => {
     const { gymId } = useParams();
     const queryClient = useQueryClient();
+    const [ gymMainImgUrl, setGymMainImgUrl ] = useState();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsModalOpen(false);
+    };
 
     const getGym = useQuery(["getGym"], async () => {
         const option = {
@@ -82,6 +91,17 @@ const GymDetail = () => {
         return response;
     });
 
+    const getImgs = useQuery(["getImgs"], async () => {
+        const option = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+        };
+        const response = await axios.get(`http://localhost:8080/gym/${gymId}/img`, option);
+        setGymMainImgUrl("http://localhost:8080/image/post/" + response.data[0].tempName);
+        return response;
+    });
+    
     const getLikeCount = useQuery(["getLikeCount"], async () => {
         const option = {
             headers: {
@@ -92,6 +112,7 @@ const GymDetail = () => {
         const response = await axios.get(`http://localhost:8080/gym/${gymId}/like`, option);
         return response;
     });
+
 
     const getLikeStatus = useQuery(["getLikeStatus"], async () => {
         const option = {
@@ -149,15 +170,23 @@ const GymDetail = () => {
             },
         }
     );
+    
+
+
 
     if (!getGym.isLoading)
         return (
             <div css={container}>
                 <Header search={false} />
                 <main css={main}>
-                    <div>
-                        <img css={gymImg} src={getGym.data.data.gymImgUrl} />
-                    </div>
+                    <img src={gymMainImgUrl} onClick={handleOpenModal} css={gymImg}/>
+                    {isModalOpen === true ? 
+                                            <GymImgs getImgs={getImgs} 
+                                                    isOpen={isModalOpen} 
+                                                    onRequestClose={handleCloseModal} />
+                                           : ""};
+                    
+
                     <div css={detailName}>
                         <h1 css={gymName}>{getGym.data.data.gymName}</h1>
                         <h1 css={gymAddress}>{getGym.data.data.gymAddress}</h1>
