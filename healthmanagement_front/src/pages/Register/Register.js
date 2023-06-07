137ab1c77dbd27e5a18ca92afc0d87206fc9ad94
heehee <heeheer_g@naver.com>
2023-06-07 14:41:30 +0900

    Fix/ 회원가입시 초기화 안되도록 구현

--- healthmanagement_front/src/pages/Register/Register.js
+++ healthmanagement_front/src/pages/Register/Register.js
@@ -194,127 +194,127 @@ const Register = () => {
     }));
   };
 
-  if (!registerSubmit.isLoading)
-    return (
-      <div css={container}>
-        <header css={header}>
-          <img css={mainLogo} src="/image/gymLogo.png" alt="" />
-        </header>
-        <main css={main}>
-          <div css={registerInfo}>
-            <div css={registerDetail}>
-              <label css={registerLabel}>아이디</label>
-              <input
-                css={registerInput}
-                type="text"
-                placeholder="아이디를 입력해 주세요."
-                name="username"
-                onChange={onchangeHandle}
-              />
-              <div css={errorMsg}>{errorMessage.username}</div>
-            </div>
-            <div css={registerDetail}>
-              <label css={registerLabel}>비밀번호</label>
-              <input
-                css={registerInput}
-                type="password"
-                placeholder="영문, 숫자, 특수문자 포함 8~16자를 입력해 주세요."
-                name="password"
-                onChange={onchangeHandle}
-              />
-              <div css={errorMsg}>{errorMessage.password}</div>
-            </div>
-            <div css={registerDetail}>
-              <label css={registerLabel}>이름</label>
-              <input
-                css={registerInput}
-                type="text"
-                placeholder="이름을 입력해 주세요."
-                name="name"
-                onChange={onchangeHandle}
-              />
-              <div css={errorMsg}>{errorMessage.name}</div>
-            </div>
-            <div css={registerDetail}>
-              <label css={registerLabel}>이메일</label>
-              <input
-                css={registerInput}
-                type="email"
-                placeholder="이메일을 입력해 주세요."
-                name="email"
-                onChange={onchangeHandle}
-              />
-              <div css={errorMsg}>{errorMessage.email}</div>
-            </div>
-            <div css={registerDetail}>
-              <label css={registerLabel}>생년월일</label>
-              <input
-                css={registerInput}
-                type="text"
-                placeholder="생년월일을 입력해 주세요. (ex: 1900-01-01)"
-                name="birthdate"
-                onChange={onchangeHandle}
-              />
-              <div css={errorMsg}>{errorMessage.birthdate}</div>
-            </div>
-            <div css={registerDetail}>
-              <label css={registerLabel}>전화번호</label>
-              <input
-                css={registerInput}
-                type="tel"
-                placeholder="전화번호를 입력해 주세요. (ex: 010-1234-5678)"
-                name="phone"
-                onChange={onchangeHandle}
-              />
-              <div css={errorMsg}>{errorMessage.phone}</div>
-            </div>
-            <div css={registerDetail}>
-              <div css={radioList}>
-                <div css={radio}>
-                  <div css={registerLabel} style={{ height: 20 }}>
-                    사용자
+  //   if (!registerSubmit.isLoading)
+  return (
+    <div css={container}>
+      <header css={header}>
+        <img css={mainLogo} src="/image/gymLogo.png" alt="" />
+      </header>
+      <main css={main}>
+        <div css={registerInfo}>
+          <div css={registerDetail}>
+            <label css={registerLabel}>아이디</label>
+            <input
+              css={registerInput}
+              type="text"
+              placeholder="아이디를 입력해 주세요."
+              name="username"
+              onChange={onchangeHandle}
+            />
+            <div css={errorMsg}>{errorMessage.username}</div>
+          </div>
+          <div css={registerDetail}>
+            <label css={registerLabel}>비밀번호</label>
+            <input
+              css={registerInput}
+              type="password"
+              placeholder="영문, 숫자, 특수문자 포함 8~16자를 입력해 주세요."
+              name="password"
+              onChange={onchangeHandle}
+            />
+            <div css={errorMsg}>{errorMessage.password}</div>
+          </div>
+          <div css={registerDetail}>
+            <label css={registerLabel}>이름</label>
+            <input
+              css={registerInput}
+              type="text"
+              placeholder="이름을 입력해 주세요."
+              name="name"
+              onChange={onchangeHandle}
+            />
+            <div css={errorMsg}>{errorMessage.name}</div>
+          </div>
+          <div css={registerDetail}>
+            <label css={registerLabel}>이메일</label>
+            <input
+              css={registerInput}
+              type="email"
+              placeholder="이메일을 입력해 주세요."
+              name="email"
+              onChange={onchangeHandle}
+            />
+            <div css={errorMsg}>{errorMessage.email}</div>
+          </div>
+          <div css={registerDetail}>
+            <label css={registerLabel}>생년월일</label>
+            <input
+              css={registerInput}
+              type="text"
+              placeholder="생년월일을 입력해 주세요. (ex: 1900-01-01)"
+              name="birthdate"
+              onChange={onchangeHandle}
+            />
+            <div css={errorMsg}>{errorMessage.birthdate}</div>
+          </div>
+          <div css={registerDetail}>
+            <label css={registerLabel}>전화번호</label>
+            <input
+              css={registerInput}
+              type="tel"
+              placeholder="전화번호를 입력해 주세요. (ex: 010-1234-5678)"
+              name="phone"
+              onChange={onchangeHandle}
+            />
+            <div css={errorMsg}>{errorMessage.phone}</div>
+          </div>
+          <div css={registerDetail}>
+            <div css={radioList}>
+              <div css={radio}>
+                <div css={registerLabel} style={{ height: 20 }}>
+                  사용자
+                </div>
+                <div css={radioUserTypeCheck}>
+                  <div>
+                    <input
+                      type="radio"
+                      id="user"
+                      name="userType"
+                      value={0}
+                      onClick={userTypeClickHandle}
+                    />
+                    <label htmlFor="user">user</label>
                   </div>
-                  <div css={radioUserTypeCheck}>
-                    <div>
-                      <input
-                        type="radio"
-                        id="user"
-                        name="userType"
-                        value={0}
-                        onClick={userTypeClickHandle}
-                      />
-                      <label htmlFor="user">user</label>
-                    </div>
-                    <div>
-                      <input
-                        type="radio"
-                        id="owner"
-                        name="userType"
-                        value={1}
-                        onClick={userTypeClickHandle}
-                      />
-                      <label htmlFor="owner">owner</label>
-                    </div>
+                  <div>
+                    <input
+                      type="radio"
+                      id="owner"
+                      name="userType"
+                      value={1}
+                      onClick={userTypeClickHandle}
+                    />
+                    <label htmlFor="owner">owner</label>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </main>
-        <footer css={footer}>
-          <div css={register}>
-            <button
-              css={registerButton}
-              onClick={() => {
-                registerSubmit.mutate();
-              }}
-            >
-              회원가입
-            </button>
-          </div>
-        </footer>
-      </div>
-    );
+        </div>
+      </main>
+      <footer css={footer}>
+        <div css={register}>
+          <button
+            css={registerButton}
+            onClick={() => {
+              registerSubmit.mutate();
+            }}
+          >
+            회원가입
+          </button>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Register;
